6c91754700c5eff653d258aa203c5242dca7e56f
igarfieldi <f.bethe@gmx.de>
2016-06-15 22:04:20 +0200

    Added sliding window to reduce amount of instances and improve accuracy

--- src/util/Log.java
+++ src/util/Log.java
@@ -1,18 +1,35 @@
 package util;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+
 public class Log {
-	private static boolean isEnabled = false;
+	private static boolean consoleEnabled = false;
+	private static String protocol = "";
 	
-	public static void enable() {
-		isEnabled = true;
+	public static void enableConsole() {
+		consoleEnabled = true;
 	}
 	
-	public static void disable() {
-		isEnabled = false;
+	public static void disableConsole() {
+		consoleEnabled = false;
 	}
 	
 	public static void log(String s) {
-		if(isEnabled)
+		s = "[" + new SimpleDateFormat("EEE, dd MMM YYYY HH:mm:ss z").format(new java.util.Date()) + "] - " + s;
+		protocol += s;
+		if(consoleEnabled)
 			System.out.println(s);
+	}
+	
+	public static String getProtocol() {
+		return protocol;
+	}
+	
+	public static void saveProtocol(String fileName) throws FileNotFoundException {
+		PrintWriter writer = new PrintWriter(fileName);
+		writer.println(protocol);
+		writer.close();
 	}
 }
